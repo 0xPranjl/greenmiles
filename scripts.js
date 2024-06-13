cd91a0fc8d05dc7bcbb92e573e579940561d2ee5
0xpranjl <pranjal.maurya402@gmail.com>
2024-06-13 19:20:33 +0530

    added ani2

--- scripts.js
+++ scripts.js
@@ -10,13 +10,33 @@ document.addEventListener('DOMContentLoaded', function() {
         path: './cycle1.json' // Replace with the path to your animation file
     });
 
-    var animationContainer1 = document.getElementById('lottie-animation2');
+    var animationContainer1 = document.getElementById('lottie-animation-track');
 
     var animation = lottie.loadAnimation({
-        container: animationContainer,
+        container: animationContainer1,
         renderer: 'svg',
         loop: true,
         autoplay: true,
         path: './cycle2.json' // Replace with the path to your animation file
+    });
+
+    var animationContainer2 = document.getElementById('lottie-animation-coin');
+
+    var animation = lottie.loadAnimation({
+        container: animationContainer2,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './coin.json' // Replace with the path to your animation file
+    });
+
+    var animationContainer3 = document.getElementById('lottie-animation-green');
+
+    var animation = lottie.loadAnimation({
+        container: animationContainer3,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './green.json' // Replace with the path to your animation file
     });
 });
