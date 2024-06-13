77dfa2d81be2c4d5a9bee3a996587cad604dcfe3
0xpranjl <pranjal.maurya402@gmail.com>
2024-06-13 18:58:46 +0530

    ani2

--- scripts.js
+++ scripts.js
@@ -9,4 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
         autoplay: true,
         path: './cycle1.json' // Replace with the path to your animation file
     });
+
+    var animationContainer1 = document.getElementById('lottie-animation2');
+
+    var animation = lottie.loadAnimation({
+        container: animationContainer,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './cycle2.json' // Replace with the path to your animation file
+    });
 });
